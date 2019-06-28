876bb31266c801fd40c21469286970dd58e74441
Daniel Bartholomae <daniel@bartholomae.name>
2019-06-28 23:57:00 +0200

    chore: init repo

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const commonConfig = require('./jest.common.config')
+
+module.exports = {
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  roots: [
+    '<rootDir>/src'
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest'
+  },
+  testRegex: '.*\.(test|spec)\\.(t|j)sx?$'
+}
